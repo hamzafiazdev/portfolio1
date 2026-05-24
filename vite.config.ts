77e4da53e6117904205694416144775e8372b3b6
hamzafiazdev <hamzafiaz.dev@gmail.com>
2026-05-24 11:57:00 +0500

    Set GitHub Pages base path

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.GITHUB_ACTIONS ? "/portfolio/" : "/",
+  base: process.env.GITHUB_ACTIONS ? "/portfolio1/" : "/",
   plugins: [react()],
   build: {
     chunkSizeWarningLimit: 1000,
